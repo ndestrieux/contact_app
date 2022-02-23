0f65ca40827565bd967dd208c060a56fdb731561
ndestrieux <n.destrieux@gmail.com>
2022-02-23 09:37:40 +0100

    Django package updated to version 4.0 - issues related to migration fixed

--- contacts/contacts_app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from contacts_app.models import Person, Address, Phone, Email, Group
-
-admin.site.register(Person)
-admin.site.register(Address)
-admin.site.register(Phone)
-admin.site.register(Email)
-admin.site.register(Group)
--- contacts/contacts_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ContactsAppConfig(AppConfig):
-    name = 'contacts_app'
