7b91b83329e519ae22c770b83b37110d196f8e93
ndestrieux <n.destrieux@gmail.com>
2023-01-29 20:41:46 +0100

    docker compose for production created, nginx configured and static file settings fixed

--- contacts/settings.py
+++ contacts/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_URL = "static/"
+STATIC_URL = "staticfiles/"
 
 STATIC_DIR = (
     os.path.join(BASE_DIR, "contacts_app/static"),
