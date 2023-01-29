2012f34a95fe9b770e1dcae1e040371452ff8392
ndestrieux <n.destrieux@gmail.com>
2023-01-29 19:40:44 +0100

    Static settings fixed

--- contacts/settings.py
+++ contacts/settings.py
@@ -141,11 +141,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
 STATIC_DIR = (
     os.path.join(BASE_DIR, "contacts_app/static"),
-    os.path.join(BASE_DIR, "staticfiles"),
 )
 
 # https://warehouse.python.org/project/whitenoise/
